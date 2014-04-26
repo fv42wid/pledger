1b3c127871c2fff1274a850744f551e1313a185b
Frank <fv42wid@yahoo.com>
2014-04-26 11:50:59 -0400

    fresh rails app, index page appears

---
+++ app/controllers/trackees_controller.rb
@@ -0,0 +1,7 @@
+class TrackeesController < ApplicationController
+
+  def index
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pledger::Application.config.secret_token = 'd9d2f110ad47ac28fea7f7a39f014ddea0eff19681df91914e43faf8a8f917227784fc2fbe08edd24d603414d6115ef968140eb6822c7c36f2cc016386da03c3'
